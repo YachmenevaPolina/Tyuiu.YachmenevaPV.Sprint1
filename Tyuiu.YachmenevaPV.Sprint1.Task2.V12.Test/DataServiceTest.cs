21d697520893926b381361f404e50c98d3b6969b
Tyuiu.YachmenevaPV@outlook.com <poliviya7@gmail.com>
2025-09-27 20:25:32 +0500

    Task2

---
+++ Tyuiu.YachmenevaPV.Sprint1.Task2.V12.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.YachmenevaPV.Sprint1.Task2.V12.Lib;
+namespace Tyuiu.YachmenevaPV.Sprint1.Task2.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            int y = 4;
+            int z = 8;
+            var res = ds.CalculateParallelepipedVolume(x, y, z);
+            Assert.AreEqual(64, res);
+        }
+    }
+}
---
+++ Tyuiu.YachmenevaPV.Sprint1.Task2.V12/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.YachmenevaPV.Sprint1.Task2.V12.Lib;
+{
+    DataService ds = new DataService();
+
+    Console.Title = "Спринт #1 | Выполнила: Ячменёва П. В. | РППб-25-1";
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* Спринт #1                                                               *");
+    Console.WriteLine("* Тема: Арифметические операторы в С#.                                    *");
+    Console.WriteLine("* Задание #2                                                              *");
+    Console.WriteLine("* Вариант #12                                                             *");
+    Console.WriteLine("* Выполнила: Ячменёва Полина Викторовна | РППб-25-1                       *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* УСЛОВИЕ:                                                                *");
+    Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+    Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+    Console.WriteLine("*                                                                         *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+    Console.WriteLine("***************************************************************************");
+
+    int x;
+    Console.WriteLine("Введите значение Х:");
+    x = Convert.ToInt32(Console.ReadLine());
+
+    int y;
+    Console.WriteLine("Введите значение Y:");
+    y = Convert.ToInt32(Console.ReadLine());
+
+    int z;
+    Console.WriteLine("Введите значение Z:");
+    z = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+    Console.WriteLine("***************************************************************************");
+
+    Console.WriteLine("Объём параллелепипеда = " + ds.CalculateParallelepipedVolume(x, y, z));
+    Console.ReadLine();
+
+
+
+}
